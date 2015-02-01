93af88cc86e87b3aaf2944b3a27e7f1c2126f166
Gabriel Ferreira <gferreir@cs.cmu.edu>
2015-02-01 13:49:15 -0500

    task 2 - bug 1 win message > buffer replacing I for 1

---
+++ write2.asm
@@ -0,0 +1,44 @@
+section .text
+global _start
+
+_start:
+
+    xor eax, eax
+    push eax
+    push byte 't'
+    push word 'tx'
+    push dword 'bar.'
+    mov ebx, esp
+;create the file
+    mov  dx, $0666
+    mov  cx, $0101
+    ;mov  ebx, file_name
+    mov  al, 8
+    int  0x80           ;call kernel
+
+; write into the file
+   mov  ebx, eax
+   xor edx, edx
+   mov  dl, 9
+
+   xor eax, eax
+   push eax
+   push dword 'win!'
+   push word 'I '
+   mov ecx, esp
+   ;mov  ecx, msg
+   xor eax, eax
+   mov  al, 4
+   int  0x80           ;call kernel
+
+ ; close the file
+   xor ebx, ebx
+   xor eax, eax
+   mov al, 6
+   int  0x80
+
+   xor ebx, ebx
+   xor eax, eax
+   mov al, 1
+   int 0x80
+
